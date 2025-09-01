7ae6a9c839c630fed170db67b54091faa8c28ce2
Ivan Walulya <ivan.walulya@oracle.com>
2025-09-01 16:22:29 +0200

    work around

--- src/hotspot/share/gc/g1/g1CollectedHeap.cpp
+++ src/hotspot/share/gc/g1/g1CollectedHeap.cpp
@@ -1207,6 +1207,7 @@ G1CollectedHeap::G1CollectedHeap() :
   _cr(nullptr),
   _task_queues(nullptr),
   _partial_array_state_manager(nullptr),
+  _is_shutting_down(false),
   _ref_processor_stw(nullptr),
   _is_alive_closure_stw(this),
   _is_subject_to_discovery_stw(this),
@@ -1505,8 +1506,8 @@ jint G1CollectedHeap::initialize() {
   return JNI_OK;
 }
 
-bool G1CollectedHeap::concurrent_mark_is_terminating() const {
-  return _cm_thread->should_terminate();
+bool G1CollectedHeap::is_shutting_down() const {
+  return Atomic::load_acquire(&_is_shutting_down);
 }
 
 void G1CollectedHeap::stop() {
@@ -1516,6 +1517,7 @@ void G1CollectedHeap::stop() {
   _cr->stop();
   _service_thread->stop();
   _cm_thread->stop();
+  Atomic::release_store_fence(&_is_shutting_down, true);
 }
 
 void G1CollectedHeap::safepoint_synchronize_begin() {
