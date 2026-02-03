90d4bf3e00f8a2ae0a19c4b46c612da26dccd3c0
Ivan Walulya <ivan.walulya@oracle.com>
2026-02-03 15:25:45 +0100

    flatten

--- src/hotspot/share/memory/iterator.inline.hpp
+++ src/hotspot/share/memory/iterator.inline.hpp
@@ -105,6 +105,7 @@ private:
   class Table {
   private:
     template <typename KlassType, typename T>
+    ATTRIBUTE_FLATTEN
     static void oop_oop_iterate(OopClosureType* cl, oop obj, Klass* k) {
       ((KlassType*)k)->KlassType::template oop_oop_iterate<T>(obj, cl);
     }
